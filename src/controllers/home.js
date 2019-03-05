9ce76e064482cc2b710eff3a891150154c4010b8
=heider <heider@MacBook-Pro-de-HeiderZapa.local>
2019-03-05 10:35:14 -0500

    updated latest

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -7,30 +7,28 @@ ctrl.index = (req, res) => {
 };
 
 
-ctrl.translate = (req, res) => {
+ctrl.translate = async (req, res) => {
 
   var datos  = req.body.data;
 
-  console.log(datos);
+  try {
 
-  // axios.post('https://carinalab.co:3000/api/carinaligth/v1/inputText', datos)
-  axios.post('https://echo-2018.appspot.com/api/carinaligth/v1/inputText', datos)
-    .then(response => {
-      // console.log(response)
-      if (response) {
-        // console.log('Respuesta desde Carina: ', response)
-        if (response.data.response.estado.codigo === 200) {
-          // console.log('se llama a actualizarAnalisis')
-          return response.data.response;
-        } else {
-          // console.log('se llama a actualizarAnalisisSinResultados')
-          return res.sendStatus(404);
-        }
-      }
-    })
-    .catch(function (error) {
-      console.log('algo no fue bien...' + error);
-    })
+    // axios.post('https://carinalab.co:3000/api/carinaligth/v1/inputText', datos)
+    const response = await axios.post('https://echo-2018.appspot.com/api/carinaligth/v1/inputText', datos)
+    // console.log(response)
+    // console.log('Respuesta desde Carina: ', response)
+    if (response.data.response.estado.codigo === 200) {
+      // console.log('se llama a actualizarAnalisis')
+      return response.data.response;
+    } else {
+      // console.log('se llama a actualizarAnalisisSinResultados')
+      return res.sendStatus(404);
+    }
+   
+  } catch (e) {
+    console.error(e); // 💩
+  }
+
 };
 
 module.exports = ctrl;
--- src/public/js/client.js
+++ src/public/js/client.js
@@ -1,66 +1,63 @@
 //webkitURL is deprecated but nevertheless
 URL = window.URL || window.webkitURL;
 
-	var recordButton = document.getElementById('recordButton');
-	var stopButton = document.getElementById('stopButton');
+var recordButton = document.getElementById('recordButton');
+var stopButton = document.getElementById('stopButton');
 
-	var reconociendo = false;
-	var textoFinal = '';
-	var reconocimiento = null;
-	var textoIntermedio = '';
+var reconociendo = false;
+var textoFinal = '';
+var reconocimiento = null;
+var textoIntermedio = '';
 
+var gumStream; //stream from getUserMedia()
+var rec; //Recorder.js object
+var input; //MediaStreamAudioSourceNode we'll be recording
 
-	var gumStream; 						//stream from getUserMedia()
-	var rec; 							//Recorder.js object
-	var input; 							//MediaStreamAudioSourceNode we'll be recording
+// shim for AudioContext when it's not avb.
+var AudioContext = window.AudioContext || window.webkitAudioContext;
+var audioContext; //audio context to help us record
 
-	// shim for AudioContext when it's not avb.
-	var AudioContext = window.AudioContext || window.webkitAudioContext;
-	var audioContext //audio context to help us record
-
-	if (!('SpeechRecognition' in window)) {
-
-		if (!('webkitSpeechRecognition' in window)) {
-			console.log('No se logró iniciar webkitSpeechRecognition');
-		} else {
-			var reconocimiento = new window.webkitSpeechRecognition()
-		}
-
+if (!('SpeechRecognition' in window)) {
+	if (!('webkitSpeechRecognition' in window)) {
+		console.log('No se logró iniciar webkitSpeechRecognition');
 	} else {
-		var reconocimiento = new window.SpeechRecognition()
+		var reconocimiento = new window.webkitSpeechRecognition();
 	}
+} else {
+	var reconocimiento = new window.SpeechRecognition();
+}
 
+function capitalizar(s) {
+	return s.replace(`/\S/`, (m) => m.toUpperCase());
+}
 
-	function capitalizar (s) {
-		return s.replace(`/\S/`, (m) => m.toUpperCase());
-	}
+function quebrarLinea(s) {
+	return s.replace(`/\n\n/g`, '<p></p>').replace(`/\n/g`, '<br>');
+}
 
-	function quebrarLinea (s) {
-		return s.replace(`/\n\n/g`, '<p></p>').replace(`/\n/g`, '<br>');
-	}
-
-	function startRecording() {
-		/*
+function startRecording() {
+	/*
 			Simple constraints object, for more advanced audio features see
 			https://addpipe.com/blog/audio-constraints-getusermedia/
 		*/
 
-	    var constraints = { audio: true, video:false }
+	var constraints = { audio: true, video: false };
 
-	 	/*
+	/*
 	    	Disable the record button until we get a success or fail from getUserMedia()
 		*/
 
-		recordButton.disabled = true;
-		stopButton.disabled = false;
+	recordButton.disabled = true;
+	stopButton.disabled = false;
 
-		/*
+	/*
 	    	We're using the standard promise based getUserMedia()
 	    	https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
 		*/
 
-		navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-
+	navigator.mediaDevices
+		.getUserMedia(constraints)
+		.then(function(stream) {
 			/*
 				create an audio context after getUserMedia is called
 				sampleRate might change after getUserMedia is called, like it does on macOS when recording through AirPods
@@ -70,7 +67,8 @@ URL = window.URL || window.webkitURL;
 			audioContext = new AudioContext();
 
 			//update the format
-			document.getElementById("formats").innerHTML="Format: 1 channel pcm @ "+audioContext.sampleRate/1000+"kHz"
+			document.getElementById('formats').innerHTML =
+				'Format: 1 channel pcm @ ' + audioContext.sampleRate / 1000 + 'kHz';
 
 			/*  assign to gumStream for later use  */
 			gumStream = stream;
@@ -82,110 +80,101 @@ URL = window.URL || window.webkitURL;
 				Create the Recorder object and configure to record mono sound (1 channel)
 				Recording 2 channels  will double the file size
 			*/
-			rec = new Recorder(input,{numChannels:1})
+			rec = new Recorder(input, { numChannels: 1 });
 
 			//start the recording process
-			rec.record()
-
-
-		}).catch(function(err) {
-		  	//enable the record button if getUserMedia() fails
-	    	recordButton.disabled = false;
-	    	stopButton.disabled = true;
-		});
-	}
-
-	function stopRecording() {
-
-		//disable the stop button, enable the record too allow for new recordings
-		stopButton.disabled = true;
-		recordButton.disabled = false;
-
-
-		//tell the recorder to stop the recording
-		rec.stop();
-
-		//stop microphone access
-		gumStream.getAudioTracks()[0].stop();
-
-		//create the wav blob and pass it on to createDownloadLink
-		rec.exportWAV(createDownloadLink);
-	}
-
-	function createDownloadLink(blob) {
-
-		var url = URL.createObjectURL(blob);
-		var au = document.createElement('audio');
-		var li = document.createElement('li');
-		var link = document.createElement('a');
-
-		//name of .wav file to use during upload and download (without extendion)
-		var filename = new Date().toISOString();
-
-		//add controls to the <audio> element
-		au.controls = true;
-		au.src = url;
-
-		//save to disk link
-		link.href = url;
-		originalname = filename+".wav"; //download forces the browser to donwload the file using the  filename
-
-		//add the new audio element to li
-		li.appendChild(au);
-
-		//add the filename to the li
-		li.appendChild(document.createTextNode(filename+".wav "))
-
-		//upload link
-		var upload = document.createElement('a');
-		upload.innerHTML = "Upload";
-		upload.addEventListener("click", function(event){
-			  var xhr=new window.XMLHttpRequest();
-			  xhr.onload=function(e) {
-			      if(this.readyState === 4) {
-			          console.log("Server returned: ",e.target.responseText);
-			      }
-			  };
-			  var fd=new FormData();
-			  fd.append("soundBlob",blob, originalname);
-			  xhr.open("POST","/subir",true);
-			  xhr.send(fd);
+			rec.record();
 		})
-		li.appendChild(document.createTextNode (" "))//add a space in between
-		li.appendChild(upload)//add the upload link to li
+		.catch(function(err) {
+			//enable the record button if getUserMedia() fails
+			recordButton.disabled = false;
+			stopButton.disabled = true;
+		});
+}
 
-		//add the li element to the ol
-		recordingsList.appendChild(li);
-	}
+function stopRecording() {
+	//disable the stop button, enable the record too allow for new recordings
+	stopButton.disabled = true;
+	recordButton.disabled = false;
 
+	//tell the recorder to stop the recording
+	rec.stop();
 
-if(bowser.name == 'Chrome'){
+	//stop microphone access
+	gumStream.getAudioTracks()[0].stop();
 
+	//create the wav blob and pass it on to createDownloadLink
+	rec.exportWAV(createDownloadLink);
+}
+
+function createDownloadLink(blob) {
+	var url = URL.createObjectURL(blob);
+	var au = document.createElement('audio');
+	var li = document.createElement('li');
+	var link = document.createElement('a');
+
+	//name of .wav file to use during upload and download (without extendion)
+	var filename = new Date().toISOString();
+
+	//add controls to the <audio> element
+	au.controls = true;
+	au.src = url;
+
+	//save to disk link
+	link.href = url;
+	originalname = filename + '.wav'; //download forces the browser to donwload the file using the  filename
+
+	//add the new audio element to li
+	li.appendChild(au);
+
+	//add the filename to the li
+	li.appendChild(document.createTextNode(filename + '.wav '));
+
+	//upload link
+	var upload = document.createElement('a');
+	upload.innerHTML = 'Upload';
+	upload.addEventListener('click', function(event) {
+		var xhr = new window.XMLHttpRequest();
+		xhr.onload = function(e) {
+			if (this.readyState === 4) {
+				console.log('Server returned: ', e.target.responseText);
+			}
+		};
+		var fd = new FormData();
+		fd.append('soundBlob', blob, originalname);
+		xhr.open('POST', '/subir', true);
+		xhr.send(fd);
+	});
+	li.appendChild(document.createTextNode(' ')); //add a space in between
+	li.appendChild(upload); //add the upload link to li
+
+	//add the li element to the ol
+	recordingsList.appendChild(li);
+}
+
+if (bowser.name == 'Chrome') {
 	reconocimiento.continuous = true; // ideal para computador
 	reconocimiento.interimResults = true;
 	reconocimiento.lang = 'es-CO';
 
-
-	recordButton.addEventListener("click", function(){
-
+	recordButton.addEventListener('click', function() {
 		reconocimiento.start();
 
 		reconociendo = true;
 
 		//startRecording();
-
 	});
 
-	stopButton.addEventListener("click", alFinalizarReconocimiento);
+	stopButton.addEventListener('click', alFinalizarReconocimiento);
 
 	reconocimiento.onend = alFinalizarReconocimiento();
-	
-	reconocimiento.onresult = (evento) => {
+
+	reconocimiento.onresult = async (evento) => {
 		for (var i = evento.resultIndex; i < evento.results.length; ++i) {
 			if (evento.results[i].isFinal) {
-				textoFinal += event.results[i][0].transcript
+				textoFinal += event.results[i][0].transcript;
 			} else {
-				textoIntermedio += event.results[i][0].transcript
+				textoIntermedio += event.results[i][0].transcript;
 			}
 		}
 
@@ -193,13 +182,18 @@ if(bowser.name == 'Chrome'){
 			textoFinal = capitalizar(textoFinal);
 			textoFinal = quebrarLinea(textoFinal);
 
-			console.log(textoFinal);
-		}else{
-			console.log('recibiendo...')
+			try {
+				const response = await axios.post('/traducir', { data: textoFinal });
+				console.log(response);
+			} catch (e) {
+				console.error(e); // 💩
+			}
+		} else {
+			console.log('recibiendo...');
 		}
 
 		// console.log('alResultadoDelReconocimiento')
-	}
+	};
 
 	reconocimiento.onerror = (evento) => {
 		if (evento.error === 'no-speech') {
@@ -208,32 +202,17 @@ if(bowser.name == 'Chrome'){
 		if (evento.error === 'audio-capture') {
 			console.err(event.err);
 		}
-	}
+	};
 
-	function alFinalizarReconocimiento(){
-
+	function alFinalizarReconocimiento() {
 		reconociendo = false;
-
-		console.log("the response", textoFinal)
-
-			axios.post('/traducir', {data: textoFinal})
-			.then(response => {
-				console.log(response);
-			}).catch(err => {
-				console.error("error:" + err.message);
-			})
 
 		reconocimiento.stop();
 
 		//stopRecording();
-
 	}
-
-
-}else if(bowser.name == 'Safari'){
-
+} else if (bowser.name == 'Safari') {
 	//add events to those 2 buttons
-	recordButton.addEventListener("click", startRecording);
-	stopButton.addEventListener("click", stopRecording);
-	
+	recordButton.addEventListener('click', startRecording);
+	stopButton.addEventListener('click', stopRecording);
 }
